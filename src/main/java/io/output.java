0795da9b9140d7b7d7b4b5f5910b00e2e948dfe0
min174 <69948652+min174@users.noreply.github.com>
2022-10-09 20:57:21 +0100

    if/else to switch

--- src/main/java/io/output.java
+++ src/main/java/io/output.java
@@ -46,44 +46,46 @@ public class output {
         for(i=0;i<8;i++) {
             System.out.print(8-i + "|");
             for(j=0;j<8;j++) { //board[][] array values represent chess pieces
-                if(game.board[i][j] == 0) { //if square is blank
-                    System.out.print("     |");
-                }
-                if(game.board[i][j] == 1) { //white pawn
-                    System.out.print("  P  |");
-                }
-                if(game.board[i][j] == 11) { //black pawn
-                    System.out.print(" *P* |");
-                }
-                if(game.board[i][j] == 2) { //white knight
-                    System.out.print("  N  |");
-                }
-                if(game.board[i][j] == 12) { //black knight
-                    System.out.print(" *N* |");
-                }
-                if(game.board[i][j] == 3) { //white bishop
-                    System.out.print("  B  |");
-                }
-                if(game.board[i][j] == 13) { //black bishop
-                    System.out.print(" *B* |");
-                }
-                if(game.board[i][j] == 4) { //white rook
-                    System.out.print("  R  |");
-                }
-                if(game.board[i][j] == 14) { //black rook
-                    System.out.print(" *R* |");
-                }
-                if(game.board[i][j] == 5) { //white queen
-                    System.out.print("  Q  |");
-                }
-                if(game.board[i][j] == 15) { //black queen
-                    System.out.print(" *Q* |");
-                }
-                if(game.board[i][j] == 6) { //white king
-                    System.out.print("  K  |");
-                }
-                if(game.board[i][j] == 16) { //black king
-                    System.out.print(" *K* |");
+
+                switch(game.board[i][j]){
+                    case 0://if square is blank
+                        System.out.print("     |");
+                        break;
+                    case 1: //white pawn
+                        System.out.print("  P  |");
+                        break;
+                    case 11://black pawn
+                        System.out.print(" *P* |");
+                        break;
+                    case 2: //white knight
+                        System.out.print("  N  |");
+                        break;
+                    case 12://black knight
+                        System.out.print(" *N* |");
+                        break;
+                    case 3://white bishop
+                        System.out.print("  B  |");
+                        break;
+                    case 13://black bishop
+                        System.out.print(" *B* |");
+                        break;
+                    case 4: //white rook
+                        System.out.print("  R  |");
+                        break;
+                    case 14://black rook
+                        System.out.print(" *R* |");
+                        break;
+                    case 5://white queen
+                        System.out.print("  Q  |");
+                        break;
+                    case 15://black queen
+                        System.out.print(" *Q* |");
+                        break;
+                    case 6://white king
+                        System.out.print("  K  |");
+                        break;
+                    case 16://black king
+                        System.out.print(" *K* |");
                 }
             }
             System.out.print("\n  ----- ----- ----- ----- ----- ----- ----- -----\n");
